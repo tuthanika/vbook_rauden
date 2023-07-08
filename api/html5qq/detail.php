3f5edd740411004f165b8597a334f9ddd99cee87
tuthanika <tuthanika@gmail.com>
2023-07-08 11:22:32 +0700

    Update detail.php

--- api/html5qq/detail.php
+++ api/html5qq/detail.php
@@ -4,12 +4,12 @@
         die();
     }
     $book_id = $keyword;
-
     $link_url = "https://bookshelf.html5.qq.com/qbread/api/novel/intro-info?bookid=" . $book_id;
     $source = "https://bookshelf.html5.qq.com/qbread/intro?bookid=".$book_id;
     $curl = curl_init($link_url);
     $headers = array(
-        "Referer: https://bookshelf.html5.qq.com/qbread/adread/catalog",
+        "user-agent":"Mozilla/5.0 (Linux; Android 10; MI 8 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.101 Mobile Safari/537.36",
+        "Referer":"https://bookshelf.html5.qq.com/qbread",
     );
     curl_setopt_array($curl, array(
         CURLOPT_HTTPHEADER => $headers,
@@ -20,8 +20,8 @@
     $resp = curl_exec($curl);
     curl_close($curl);
     $response1 = json_decode($resp, true);
-    $book = $response1['data'];
-
+	$data = $response1['data'];
+    $book = $data['bookInfo'];
     $book_name = $book['resourceName'];
     $author = $book['author'];
     // $free_status = $res_json['data']['book_info']['free_status'];
